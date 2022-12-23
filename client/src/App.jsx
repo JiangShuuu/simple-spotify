71f69dc560bdddde1391c062bf5410f91e708288
JiangShuuu <globelex65@gmail.com>
2022-12-23 16:12:56 +0800

    upd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import Login from './Login';
+import Home from './Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/Login' element={<Login />}></Route>
       </Routes>
     </BrowserRouter>
--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from 'react'
+import { Container } from 'react-bootstrap'
 
-const AUTH_URL = import.meta.env.AUTH_ENDPOINT;
+const AUTH_URL = import.meta.env.VITE_AUTH_ENDPOINT
 
-console.log(AUTH_URL);
+console.log(AUTH_URL)
 export default function Login() {
-  return <div>Login</div>;
+  const URL = `${AUTH_URL}?client_id=${
+    import.meta.env.VITE_CLIENT_ID
+  }&response_type=code&redirect_uri=http://localhost:5173&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: '100vh' }}
+    >
+      <a className="btn btn-success btn-lg" href={URL}>
+        Login with Spotify
+      </a>
+    </div>
+  )
 }

12ef9663615613f987dbdb5d426cc65be84234e1
JiangShuuu <globelex65@gmail.com>
2022-12-24 11:42:05 +0800

    upd

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const cors = require("cors")
+const bodyParser = require("body-parser")
 const spotifyWebApi = require('spotify-web-api-node')
 
 const app = express()
 
+app.use(cors())
+app.use(bodyParser.json())
+
 app.post('/login', (req, res) => {
   const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
+  const spotifyApi = new spotifyWebApi({
     redirectUri: 'http://localhost:3000',
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
@@ -17,7 +22,10 @@ app.post('/login', (req, res) => {
       refreshToken: data.body.refreshToken,
       expiresIn: data.body.expires_in
     })
-  }).catch(() => {
+  }).catch((err) => {
+    console.log(err)
     res.sendStatus(400)
   })
 })
+
+app.listen(3000)
